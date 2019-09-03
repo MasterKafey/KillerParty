eb46792f8ef34c423e0adf6cf812a129208fae48
MasterKafei <MasterKafei59@gmail.com>
2019-09-03 02:57:23 +0200

    [Update] Update user fixtures

--- src/AppBundle/DataFixtures/ORM/DataUserFixtures.php
+++ src/AppBundle/DataFixtures/ORM/DataUserFixtures.php
@@ -4,12 +4,20 @@ namespace AppBundle\DataFixtures\ORM;
 
 
 use AppBundle\Entity\User;
+use AppBundle\Service\Business\UserBusiness;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class DataUserFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
+    private $userBusiness;
+
+    public function __construct(UserBusiness $business)
+    {
+        $this->userBusiness = $business;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
@@ -18,11 +26,12 @@ class DataUserFixtures extends AbstractFixture implements OrderedFixtureInterfac
             ->setFirstName('Jean')
             ->setLastName('Marius')
             ->setEmail('jean.marius@supinfo.com')
-            ->setPlainPassword('admin')
             ->setRoles(array(
                 'ROLE_ADMIN'
             ))
             ;
+        $this->userBusiness->generatePasswordToken($user, false);
+        $this->userBusiness->generatePhoneToken($user, false);
 
         $manager->persist($user);
         $manager->flush();
